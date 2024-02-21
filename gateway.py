782c53225562d3c2a8a3e5b471d3e0832b05ec5a
Leon Kipkoech <leonkipkip@gmail.com>
2024-02-21 16:03:09 -0500

    API + Gateway

--- gateway.py
+++ gateway.py
@@ -2,35 +2,16 @@
 # encoding: utf-8
 import json
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 from credentialsAPI import CredentialsAPIservice
 
 app = Flask(__name__)
-@app.route('/')
-def index():
-    return jsonify({'name': 'alice',
-                    'email': 'alice@outlook.com'})
+CORS(app)  # Enable CORS for all routes
 
-@app.route('/save_credentials', methods=['POST'])
-def save_credentials():
-    # receives credentials.json 
-    # writes credentials.json data to .env/credentials.json
-    credentials_file = request.args.get('credentials')
-    if not credentials_file:
-        return credentials_file({'error': 'File is invalid'}), 400
-    
-    # Check if a file was sent in the request
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file provided in the request'}), 400
-    
-    uploaded_file = request.files['file']
-    
-    # Specify the path where you want to save the file
-    save_path = f'.env/{uploaded_file.filename}'
-    
-    # Save the uploaded file to the specified location
-    uploaded_file.save(save_path)
-    return jsonify({'success': 'credential file saved'})
+@app.route('/check_application', methods=['GET'])
+def check_application():
+    return jsonify({"success": "application is running"}), 200
 
 @app.route('/get_credentials', methods=['GET'])
 def get_credentials():
@@ -39,15 +20,15 @@ def get_credentials():
     try:
         credentials = CredentialsAPIservice.getCredentials()
     except:
-        return jsonify({"error": "there was an error fetching credentials"})
+        return jsonify({"error": "there was an error fetching credentials"}), 400
     print(credentials)
-    return jsonify({'success': 'credentials verified'})
+    return jsonify({'success': 'credentials verified'}), 200
 
 @app.route('/save_settings', methods=['POST'])
 def save_settings():
     # receives credentials.json 
     # writes credentials.json data to .env/credentials.json
-    settings = request.args.get('results')
+    settings = request.json.get('results')
     if not settings:
         return jsonify({'error': 'settings parameter is required'}), 400
     
@@ -55,28 +36,10 @@ def save_settings():
     if not request.is_json:
         return jsonify({'error': 'No JSON data provided in the request'}), 400
     
-    # Extract JSON data from the request body
-    json_data = request.json
-    
-    # Assuming your JavaScript map is directly sent in the JSON data
-    if settings in json_data:
-        return jsonify(json_data[settings])
-    else:
-        return jsonify({'error': 'data not found for provided location'}), 404
+    with open('./components/settings.json', 'w') as f:
+        json.dump(settings, f)
+    return jsonify({'success':"data has been received"}), 200
 
 
-
-@app.route('/', methods=['PUT'])
-def create_record():
-    record = json.loads(request.data)
-    with open('/tmp/data.txt', 'r') as f:
-        data = f.read()
-    if not data:
-        records = [record]
-    else:
-        records = json.loads(data)
-        records.append(record)
-    with open('/tmp/data.txt', 'w') as f:
-        f.write(json.dumps(records, indent=2))
-    return jsonify(record)
-app.run()
+if __name__ == '__main__':
+    app.run()
